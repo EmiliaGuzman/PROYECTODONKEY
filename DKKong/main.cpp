70412e54df72911b61be42a846b98ef11412094f
EmiliaGuzman <54367844+EmiliaGuzman@users.noreply.github.com>
2019-11-29 10:41:29 -0500

    Add files via upload

---
+++ DKKong/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "SFML/Graphics.hpp"
+#include "Game.hpp"
+
+int main()
+{
+  Game* game = new Game();
+
+  game->setUpSystem("Donkey Kong",1000,600);
+
+  while(game->loop());
+
+  delete game;
+
+  return 0;
+}
